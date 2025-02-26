462a2436d693271e37ea8d16884074b0d0401c72
Valeriy Semenenko <gearmobile@gmail.com>
2025-02-26 18:32:29 +0300

    feat: :sparkles: add write method for mongodb

--- api/src/helpers/connect_db.js
+++ api/src/helpers/connect_db.js
@@ -6,7 +6,6 @@ export const connectDB = () => {
   try {
     mongoose.connect(url);
   } catch (err) {
-    console.error(err.message);
     process.exit(1);
   }
 
--- api/src/index.js
+++ api/src/index.js
@@ -2,25 +2,35 @@ import express from 'express';
 
 import { connectDB } from './helpers/connect_db.js';
 import { port } from './configurations/index.js';
+import { BookModel } from './model/book.model.js';
 
 const app = express();
 
-app.get('', (_, res) => {
+app.get('', async (_, res) => {
   res.redirect('/test');
 });
 
-app.get('/test', (_, res) => {
+app.get('/test', async (_, res) => {
   res.send('Hello from Express!');
 });
 
 const startServer = () => {
-  app.listen(port, () => {
+  app.listen(port, async () => {
     console.log(`Server start on http://localhost:${port}`);
+    const book = new BookModel({
+      title: 'The Catcher in the Rye',
+      author: 'J.D. Salinger',
+      year: 1951,
+    });
+    await book.save();
+    /** get all books form bd */
+    const books = await BookModel.find({});
+    console.log(books);
   });
 };
 
 connectDB()
-  .on('error', (err) => {
+  .on('error', async (err) => {
     console.error(`connection error: ${err}`);
   })
   .on('disconnected', connectDB)
